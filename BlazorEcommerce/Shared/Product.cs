a14c8e71c0212f1c8904c090e7e806ee3873239f
Iain Stewart <iain.stewart@kpmg.co.uk>
2023-02-14 07:57:10 +0000

    Add migration for Product

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -8,6 +8,7 @@ namespace BlazorEcommerce.Shared
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
