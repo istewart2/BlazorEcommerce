794cb44171f3f57a1bae7dbdc5826737b6b12e3e
Iain Stewart <iain.stewart@kpmg.co.uk>
2023-02-16 21:24:48 +0000

    Demo how to use js interop

---
+++ BlazorEcommerce/Client/wwwroot/js/common.js
@@ -0,0 +1,9 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Success!", {timeout: 5000});
+    }
+
+    if (type === "error") {
+        toastr.error(message, "Error!", { timeout: 5000 });
+    }
+}
