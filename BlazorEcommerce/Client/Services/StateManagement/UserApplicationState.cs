179da65b5eb259a70641d30475b6fe7731c070a7
Iain Stewart <iain.stewart@kpmg.co.uk>
2023-02-17 12:09:07 +0000

    Add cascading parameter sample

---
+++ BlazorEcommerce/Client/Services/StateManagement/UserApplicationState.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorEcommerce.Client.Services.StateManagement
+{
+    // can declare multiple state objects (like stores), doesn't need to be global
+    // could also use DI to inject a state container service
+
+    // we could persist this object in browser storage, but using protected browser storage
+    // is only supported for blazor server
+    public class UserApplicationState
+    {
+        public int CounterValue { get; set; } = 0;
+    }
+}
